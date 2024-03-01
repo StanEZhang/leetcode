92b13948a8f2fd7e054fa11454a205736e78d8de
StanEZhang <2933433780@qq.com>
2024-03-01 16:37:33 +0800

    移动零

--- src/com/haibin/leetcode/中等/最长连续序列.java
+++ src/com/haibin/leetcode/中等/最长连续序列.java
@@ -11,12 +11,35 @@ import java.util.HashSet;
  * @author: zhanghaibin
  * @create: 2023-09-22
  */
+
+/**
+ * 第一步：
+ * 把这个数组元素全部扔进HashSet去重，去重是一个目的。
+ * 还有一个目的是为了查找元素复杂度O(1)
+ *
+ * 我们题目要找最长连续数列的长度。
+ * 问题就转化为我们要找到最长连续数列。
+ * 问题就转化为我们要找到这个序列的头儿。
+ * 只要找到头儿，头一直+1+1，挨个找就能找到尾巴。
+ * 那头儿的标志是什么？
+ *
+ * 如果这个X,在HashSet里找不到X-1,也就是没有比它更小的了。
+ * 意味着X要么是孤儿，要么他就是序列的头儿。
+ * 然后我们再while循环找X+1,一直到找不到为止，找到序列的尾巴。
+ * 过程中计数，算出序列长度。
+ * 然后之前我们必须记录一个最大长度。
+ * 然后这两个长度比较的最大值才是最终的最大长度。
+ *
+ * 回到上一段分析的开头，那这个X要是在HashSet里找到了X-1说明什么？
+ * 说明它不是这个序列头啊。
+ * 直接跳过就行了
+ */
 public class 最长连续序列 {
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-        // 数组转换成哈希set
+        // 数组转换成哈希set，去重
         HashSet<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
--- src/com/haibin/leetcode/简单/移动零.java
+++ src/com/haibin/leetcode/简单/移动零.java
@@ -9,8 +9,63 @@ import java.util.Arrays;
  * @author: zhanghaibin
  * @create: 2023-06-28
  */
-public class 移动零 {
 
+/**
+ * 这道题的话看长相有点像冒泡。
+ * 你看这个思路就应该是什么呢？
+ * 遍历这个数组，如果你是0的话，就跳过去。
+ * 如果你不是0，就往上也就是往左冒泡。
+ * 说冒泡是因为形象一些，实际上的话就是循环换位置。
+ * 这里需要注意的问题就是：
+ * 比方说你1冒泡到顶上了。
+ * 那你第二个非零的数3往上冒泡就不能冒泡到最顶，就得在1下边。
+ * 那怎么实现呢？
+ * 比较简单，就是每次循环到非0的数就记一次。
+ * 还是比较好理解。
+ * 下面就写一下实现。
+ * 实现过程中发现，冒泡的过程中不需要逐个交换位置，直接换一次就可以了。
+ * 因为当前非0的数到上一个非0的数之间一定全是0.
+ *
+ * 按moveZeroes2提交有报错，原因是没有控制边界条件。
+ * 第二次提交错误，原因是交换位置没有按正常程序走，想当然了。
+ */
+
+public class 移动零 {
+    /**
+     * @date: 2024/03/01
+     输入: nums = [0,1,0,3,12]
+     输出: [1,3,12,0,0]
+     */
+    /**
+        冒泡过程
+     0 1 0 3 12
+     1 0 0 3 12
+     1 3 0 0 12
+     1 3 12 0 0
+
+     */
+    public int[] moveZeroes2(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+
+        // 记录非0的数
+        int n = 0;
+
+        // 1,0,0,3
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0){
+                // 冒泡,交换位置
+                int temp = nums[n];
+                nums[n] = nums[i];
+                nums[i] = temp;
+                n++;
+            }
+
+        }
+        return nums;
+
+    }
     /**
      输入: nums = [0,1,0,3,12]
      输出: [1,3,12,0,0]
@@ -36,9 +91,9 @@ public class 移动零 {
     }
 
     public static void main(String[] args) {
-        int[] nums = {0, 1};
+        int[] nums = {1, 0};
         移动零 移动零 = new 移动零();
-        int[] ints = 移动零.moveZeroes(nums);
+        int[] ints = 移动零.moveZeroes2(nums);
         System.out.println(Arrays.toString(ints));
     }
 }
